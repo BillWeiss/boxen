ea626174c80b4b3b0562a415166f06526f41af3f
Bill Weiss <bweiss@backstopsolutions.com>
2015-04-24 10:08:54 -0500

    Moving my personal content into modules/people
    Easy enough

--- manifests/site.pp
+++ manifests/site.pp
@@ -84,35 +84,4 @@ node default {
     ensure => link,
     target => $boxen::config::repodir
   }
-
-  # local modifications
-  include spotify
-  include chrome
-  include macvim
-# says it doesn't conflict with macvim.  Lies.
-#  include vim
-  include iterm2::dev
-#  include atom # busted, I don't know.  https://github.com/boxen/puppet-atom/issues/4
-  include quicksilver
-  include tunnelblick::beta
-  include firefox
-  include steam
-  include python
-
-  package {
-    [
-      'evernote',
-    ]:
-    provider => 'brewcask',
-  }
-
-  git::config::global { 'user.email':
-      value => 'bweiss@backstopsolutions.com';
-    'user.name':
-      value => 'Bill Weiss';
-  }
-
-  git::config::global { 'push.default':
-    value => 'matching',
-  }
 }
